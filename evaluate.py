10a9974666718e5fa39a1c94219bf85bc47781d3
Zreal <zenglz_pro@163.com>
2020-10-27 18:36:50 +0800

    finsih rand index

--- evaluate.py
+++ evaluate.py
@@ -4,14 +4,24 @@
  * @create date 2020-10-26 16:36:19
  * @desc 
 '''
-from os import name
-from typing import Any, Optional, Sequence, Tuple, overload
+import itertools
+from typing import Any, Dict, Optional, Sequence, Tuple, overload
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics.classification import recall_score
 from torch.backends.mkldnn import set_flags
-
+import numpy as np
+from scipy.special import comb
 
 class EvalUnit(object):
+    """Smalled Evaluating metrics unit
+    Attribute:
+        tp: True Positive item
+        fp: False Positive item
+        fn: False Negative item
+        tn: True Negative item
+        name: A label to describe specific instance
+    """
+
     def __init__(self, tn:int=0, fp:int=0, fn:int=0, tp:int=0, name:Optional[int]=None) -> None:
         super(EvalUnit,self).__init__()
         self.name = name if name is not None else "None"
@@ -70,7 +80,81 @@ def binary_confusion_matrix_evaluate(y_true:Sequence[Any], y_pred:Sequence[Any])
     # import pdb; pdb.set_trace()
     tn ,fp, fn, tp =  confusion_matrix(y_true,y_pred).ravel()
     return EvalUnit(tn,fp,fn,tp)
+
+
+""" ---------------- cluster Metrics ---------------- """
+def rand_index_plus(pred_cluster:Dict[Any], target_cluster:Dict[Any]) -> EvalUnit:
+    """Calcultae the rand index and Get detailed result
+    RI = Accuracy = (TP+TN)/(TP,TN,FP,FN)
+    Args:
+        pred_cluster: Dict element:cluster_id （cluster_id from 0 to max_size）| predicted clusters 
+        target_cluster: Dict element:clutser_id （cluster_id from 0 to max_size) | target clusters  
+    Returns:
+        In order to return detailed data, It will return a EvalUnit, 
+        call accuracy to get RI.
+    """
+    pred_elements = list(pred_cluster.keys())
+    target_elements = list(target_cluster.keys())
+    it = itertools.product(pred_elements,target_elements)
+    tp,fp,tn,fn = 0,0,0,0
+    for x,y in it:
+        if x != y:#other word
+            x_cluster = pred_cluster[x]
+            x_cluster_ = target_cluster[x]
+            y_cluster = pred_cluster[y]
+            y_cluster_ = target_cluster[y]
+
+            if x_cluster == y_cluster and x_cluster_ == y_cluster_:
+                tp += 1
+            elif x_cluster != y_cluster and x_cluster_ != y_cluster_:
+                tn += 1
+            elif x_cluster == y_cluster and x_cluster_ != y_cluster_:
+                fp += 1
+            else:
+                fn +=1
+    return EvalUnit(tp,tn,fp,fn,'rand_index')
+
+
+def rand_index(pred_cluster: Dict[Any], target_cluster: Dict[Any]) -> float:
+    """Use contingency_table to get RI directly
+    Args:
+        pred_cluster: Dict cluster_id: List[element] （cluster_id from 0 to max_size）| predicted clusters 
+        target_cluster: Dict cluster_id: List[element] （cluster_id from 0 to max_size) | target clusters  
+    Return:
+        RI (float)
+    """
+    pred_cluster_size = len(pred_cluster)
+    target_cluster_size = len(target_cluster)
+    contingency_table = np.zeros((pred_cluster,target_cluster))
     
+    for i, p_cluster in enumerate(pred_cluster):
+        for j, t_cluster in enumerate(target_cluster):
+            #find common element
+            l = [*p_cluster,*t_cluster]
+            contingency_table[i][j] = len(l) - len(set(l))
+    s = comb(np.sum(contingency_table), 2)
+    a = 0
+    for i in np.nditer(contingency_table):
+        a += comb(i,2)
+    return a/s
+
+def adjusted_rand_index(pred_cluster:Dict[Any], target_cluster:Dict[Any]):
+    """Docstring
+    """
+    pred_cluster_size = len(set(pred_cluster.values()))
+    target_cluster_size = len(set(target_cluster.values()))
+    contingency_table = np.zeros((pred_cluster_size, target_cluster_size))
+    s = comb(np.sum(contingency_table), 2)
+    ij = 0
+    for i in np.npiter(contingency_table):
+        ij += comb(i,2)
+    pred_sum = np.sum(contingency_table, axis=1)
+    target_sum = np.sum(contingency_table, aixs=0)
+    
+    
+    pass
+
+
 
 if __name__ == '__main__':
     print(1)
