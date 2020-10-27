90673acf37f7d36cd3cce934a8877434d9fdf101
Zreal <zenglz_pro@163.com>
2020-10-27 16:36:21 +0800

    finsih trian test validation method

--- wrapper.py
+++ wrapper.py
@@ -6,19 +6,24 @@
 @desc [description]
 '''
 from datetime import datetime
-from logging import log
+from utils import set_padding
 import torch
 from torch import optim
-from typing import Dict, None, Optional
+from typing import Any, Dict, None, Optional, Sequence
+import numpy as np
+from torch.utils import data
 from model import Embedding_layer,Scorer,SetinstanceClassifier
-from dataloader import Dataloader, test_dataloader
+from dataloader import DataSet, Dataloader, test_dataloader
 from pathlib import Path
 from log import logger
 from tqdm import tqdm
 from copy import deepcopy
+from evaluate import EvalUnit,binary_confusion_matrix_evaluate
 
 class SynMineModelWrapper(object):
-    """Class to wrapper training and testing of deeplearning model"""
+    """Class to wrapper training and testing of deeplearning model
+    
+    """
     def __init__(self, modelconfig:Dict, trainingconfig:Dict) -> None:
         super(SynMineModelWrapper,self).__init__()
         self.device = torch.device("cuda:1") if torch.cuda.is_available() else torch.device('cpu')
@@ -31,6 +36,7 @@ class SynMineModelWrapper(object):
         self.model = SetinstanceClassifier(scorer)
         self.loss_fn = modelconfig['loss_fn']
         self.start_epoches = 0
+        self.threshold = trainingconfig['threshold']
         self.epoches = trainingconfig['epoch']
         self.checkpoint_epoches = trainingconfig['checkpoint_epoch']
         self.print_step = trainingconfig['print_step']
@@ -38,13 +44,21 @@ class SynMineModelWrapper(object):
 
         self.best_model = self.model.to(self.device)
         self.best_loss = 1e10
-
+        self.best_score = 0
         self.checkpoint_dir =trainingconfig['checkpoint_dir']
 
         self.optimizer = optim.Adam(filter(lambda p: p.requires_grad, self.model.parameters()), lr=self.lr, amsgrad=True)
 
 
     def train(self, train_dataloader:Dataloader, dev_dataloader:Optional[Dataloader] = None) -> None:
+        """Implementation to Batch Train the model
+        Args:
+            train_dataloader : training dataset iteration used to train model
+            dev_dataloader : development dataset iteration used to validate model
+                if dev_dataloader = None, it will split the trianing dataset to create dev dataset
+        Returns:
+            None
+        """
         all_step = len(train_dataloader)
         t = range(self.start_epoch, self.epoches)
 
@@ -61,7 +75,7 @@ class SynMineModelWrapper(object):
         for epoch in t:
             self.model.train()
             ep_loss = 0
-
+            summary_metrics = EvalUnit(0,0,0,0,'epoch'+str(epoch))
             for step,item in enumerate(train_dataloader_):
                 word_set, mask, new_word_set, mask_, labels = item
                 F = lambda x: torch.Tensor(x).long().to(self.device)
@@ -69,8 +83,7 @@ class SynMineModelWrapper(object):
                     i for i in [word_set, mask, new_word_set, mask_]
                 ]
                 pred_labels = self.model(word_set_tensor, mask, new_word_set_tensor, mask_)
-                #batch_size * 2 : vector
-                labels = [[1,0] if i == 0 else [0,1] for i in labels]
+                #batch_size * 1 : vector
                 labels_tensor = torch.Tensor(labels).float().to(self.device)
                 cur_loss = self.loss_fn(pred_labels,labels_tensor) / labels_tensor.shape[0]
                 ep_loss += cur_loss.item()
@@ -78,53 +91,75 @@ class SynMineModelWrapper(object):
                 self.optimizer.zero_grad()
                 cur_loss.backward()
                 self.optimizer.step()
-
+                # cal metrics
+                pred_labels = np.where(pred_labels.cpu().numpy()>self.threshold, 1, 0)
+                unit = binary_confusion_matrix_evaluate(np.array(labels),pred_labels)
+                summary_metrics += unit
                 # log  
                 if (step+1) % self.print_step == 0 or step + 1 == all_step:
                     desc = 'Epoch:{} {} / {} Loss:{}'.format(epoch,step,all_step,cur_loss.item())
                     logger.info(desc)
+            #log metrics
+            logger.info("In Training Set, Metrics:")
+            logger.info(summary_metrics)
 
             #validate
-            val_loss = self.validate(dev_dataloader_)
+            self.validate(dev_dataloader_)
+
             #save checkpoint
             if epoch + 1 == self.start_epoches + self.checkpoint_epoches:
                 #update best_model
                 self.save_check_point()
                 self.start_epoches = epoch
+                logger.info('Successfully Store Checkpoint in epoch {}'.format(epoch+1))
 
             val_loss_list.append(val_loss_list)
             ep_loss_list.append(ep_loss)
+        # tensorboard plot
+
     def validate(self,dev_dataloader:Dataloader) -> None:
-        #No dev dataset, so training dataset is validation dataset\
+        """Implementation to Batch validate the model using developed dataset
+        Args:
+            dev_dataloader : development dataset iteration used to validate model
+        """
         self.model.eval()
         val_loss = 0
+        summary_metrics = EvalUnit(name='Validation')
         for step,item in dev_dataloader:
             word_set, mask, new_word_set, mask_, labels = item
             F = lambda x: torch.Tensor(x).long().to(self.device)
             word_set_tensor, mask, new_word_set_tensor, mask_ = [
                 i for i in [word_set, mask, new_word_set, mask_]
             ]
-            labels_ = [[1,0] if i == 0 else [0,1] for i in labels]
             pred_labels = self.model(word_set_tensor, mask, new_word_set_tensor, mask_)
-            labels_tensor = torch.Tensor(labels_).float().to(self.device)
+            labels_tensor = torch.Tensor(labels).float().to(self.device)
             cur_loss = self.loss_fn(pred_labels,labels_tensor) / labels_tensor.shape[0]
             val_loss += cur_loss
-            pred_labels = torch.argmax(pred_labels, dim=1)
             #metrics
-        if val_loss < self.best_loss:
-            self.best_loss = val_loss
-            self.best_model = deepcopy(self.model)
+            pred_labels = np.where(pred_labels.cpu().numpy()>self.threshold, 1, 0)
+            unit = binary_confusion_matrix_evaluate(np.array(labels),pred_labels)
+            summary_metrics += unit
 
         #log
-        return val_loss
+        desc = 'In Validation, Average Loss:{:.2f}'.format(val_loss)
+        logger.info(desc)
+        logger.info(summary_metrics)
 
+        if summary_metrics.f1_score > self.best_score:
+            self.best_loss = val_loss
+            self.best_score = summary_metrics.f1_score
+            self.best_model = deepcopy(self.model)
+            logger.info('Successfully Update Best Model')
         
-            
-    
     def test(self, test_dataloader:Dataloader) -> None:
+        """Implementation to Batch test the model using developed dataset
+        Args:
+            test_dataloader : test dataset iteration used to test the model
+        """
         self.best_model.eval()
         self.best_model.to(self.device)
         all_step = len(test_dataloader)
+        summary_metrics = EvalUnit()
         for step,item in enumerate(test_dataloader):
             word_set, mask, new_word_set, mask_, labels = item 
             F = lambda x: torch.Tensor(x).long().to(self.device)
@@ -132,15 +167,21 @@ class SynMineModelWrapper(object):
                 i for i in [word_set, mask, new_word_set, mask_]
             ]
             pred_labels = self.best_model(word_set_tensor, mask, new_word_set_tensor, mask_)
-            pred_labels = torch.argmax(pred_labels,dim=1)
-
+            pred_labels = np.where(pred_labels.cpu().numpy()>self.threshold, 1, 0)
             # cal metrics
-            
-            if (step+1) % self.print_step == 0 or (step+1) == all_step:
-                desc = ''
-                logger.info('Test:'+desc)
+            unit = binary_confusion_matrix_evaluate(np.array(labels),pred_labels)
+            summary_metrics += unit
+
+        logger.info("In Test DataSet")
+        logger.info(summary_metrics)
     
     def load_check_point(self,file_name:Optional[str] = None) -> None:
+        """load the save model file
+        Args:
+            file_name: filename not filepath, it will concatenate the checkpoint_dir and filename 
+                to create the whole path. It filename is none, it will choose the latest checkpoint file
+                under the checkpoint directory.
+        """
         dir_path = Path(self.checkpoint_dir)
         if file_name == None:
             import os
@@ -154,15 +195,18 @@ class SynMineModelWrapper(object):
         self.model.load_state_dict(checkpoint['state_dict']).to(self.device)
         self.best_model.load_state_dict(checkpoint['best_model'])
         self.optimizer.load_state_dict(checkpoint['optimizer'])
+        self.best_score = checkpoint['best_score']
     
     def save_check_point(self) -> None:
-        """
+        """ save the key attribute in this wrapper
+        key attribute:
         d = {
             'epoch' : epoch,
             'state_dict' : model.state_dict(),
             'optimizer' : optimizer.state_dict(),
             'best_loss' : best_loss,
-            'best_pred' : best_pred
+            'best_score' : best_score,
+            'best_model' : best_pred
         }
         """
         dir_path = Path(self.checkpoint_dir)
@@ -173,7 +217,44 @@ class SynMineModelWrapper(object):
             'state_dict' : self.model.state_dict(),
             'optimizer' : self.optimizer.state_dict(),
             'best_loss' : self.best_loss,
-            'best_model' : self.best_model.state_dict()
+            'best_model' : self.best_model.state_dict(),
+            'best_score' : self.best_score
         }
         torch.save(d, filepath)
+    
+    def cluster_predict(self, dataset:DataSet, word2id:Dict) -> Sequence[Any]:
+        """Using Model to cluster wordset
+        """
+        self.best_model.eval()
+        vocab = dataset.vocab
+        words = vocab.keys()
+        wordset_list = []
+        for word in words:
+            wordid = word2id[word]
+            if not wordset_list:
+                # Empty
+                wordset_list.append([wordid])
+            new_wordset = [ i.append(wordid) for i in wordset_list]
+            old_wordset = deepcopy(wordset_list)
+            old_wordset, mask = set_padding(old_wordset)
+            new_wordset, mask_ = set_padding(new_wordset)
+            #batch_size * max_word_set_size
+            F = lambda x: torch.Tensor(x).long().to(self.device)
+            word_set_tensor, mask, new_word_set_tensor, mask_ = [
+                i for i in [old_wordset, mask, new_wordset, mask_]
+            ]
+            scores = self.best_model(word_set_tensor, mask, new_word_set_tensor, mask_)
+            index = torch.argmax(scores).item()
+            best_scores = torch.max(scores).item()
+
+            if best_scores > self.threshold:
+                wordset_list[index].append(wordid)
+        #id2word
+        id2word = { j:i for i,j in word2id.items()}
+        F = lambda x:[ id2word[i] for i in x]
+        pred_word_sets = [ F(wordset) for wordset in wordset_list]
+
+        #metrics
+
+
         
