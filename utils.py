a0bc0d2c6375cebb736cfb07bad007b1b864c446
Zreal <zenglz_pro@163.com>
2020-11-04 16:06:07 +0800

    fix bug: change helper_trans_to_labelsequence

--- utils.py
+++ utils.py
@@ -9,6 +9,8 @@ import numpy as np
 from typing import List,Tuple
 from pathlib import Path
 import numpy as np
+import random
+import torch
 
 pwd = Path.cwd()
 
@@ -28,7 +30,26 @@ def set_padding(m:List[List[int]],padding = 0)-> Tuple[np.array,np.array]:
 
 
 
-
+def set_random_seed(seed = 10, deterministic = False, benchmark = False):
+    """
+    Args:
+        seed: Random Seed
+        deterministic:
+                Deterministic operation may have a negative single-run performance impact, depending on the composition of your model. 
+                Due to different underlying operations, which may be slower, the processing speed (e.g. the number of batches trained per second) may be lower than when the model functions nondeterministically. 
+                However, even though single-run speed may be slower, depending on your application determinism may save time by facilitating experimentation, debugging, and regression testing.
+        benchmark: whether cudnn to find most efficient method to process data
+                If no difference of the size or dimension in the input data, Setting it true is a good way to speed up
+                However, if not,  every iteration, cudnn has to find best algorithm, it cost a lot
+    """
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    if deterministic:
+        torch.backends.cudnn.deterministic = True
+    if benchmark:
+        torch.backends.cudnn.benchmark = True
 
 """-------------------- Test ------------------------"""
 # def test_read_embed_info():
