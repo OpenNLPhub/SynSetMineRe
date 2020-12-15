878bbcd13c725d72c9349693aa0b14ba70ef7999
Zreal <zenglz_pro@163.com>
2020-12-15 16:28:53 +0800

    clean.sh

---
+++ clean.sh
@@ -0,0 +1,10 @@
+PROJECT_ROOT=$(pwd)
+# echo ${PROJECT_ROOT}
+
+LOG_ROOT=${PROJECT_ROOT}/log
+# echo ${LOG_ROOT}
+rm -rv ${LOG_ROOT}/*
+
+RUNS_ROOT=${PROJECT_ROOT}/runs
+# echo ${RUNS_ROOT}
+rm -rv ${RUNS_ROOT}/*
